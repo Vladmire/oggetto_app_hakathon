1d2c36a1ddf056bb637b73faf05d70774d7115ed
Vladimir Dubovik <borisonia0216@gmail.com>
2022-11-26 01:36:03 +0300

    add user model

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,18 +1,48 @@
-// import 'package:json_annotation/json_annotation.dart';
-// import 'package:oggetto_app_hakathon/models/enums.dart';
-//
-// part 'details_model.g.dart';
-//
-// @JsonSerializable()
-// class UserModel {
-//   String login;
-//   String password;
-//   String name;
-//   String surname;
-//   DepartmentType department;
-//   WorkGrade workGrade;
-//   int birthDate;
-//
-//
-//
-// }
+import 'package:json_annotation/json_annotation.dart';
+import 'package:oggetto_app_hakathon/models/enums.dart';
+
+part 'user_model.g.dart';
+
+@JsonSerializable()
+class UserModel {
+  String login;
+  String password;
+  String name;
+  String surname;
+  String? fatherName;
+  DepartmentType department;
+  WorkGrade? workGrade;
+  String? project;
+  int birthDate;
+  String? firstFact;
+  String? secondFact;
+  String? thirdFact;
+
+  UserModel({
+    required this.login,
+    required this.password,
+    required this.name,
+    required this.surname,
+    required this.birthDate,
+    required this.department,
+    this.project,
+    this.fatherName,
+    this.workGrade,
+    this.firstFact,
+    this.secondFact,
+    this.thirdFact,
+});
+
+  /// A necessary factory constructor for creating a new User instance
+  /// from a map. Pass the map to the generated `_$UserFromJson()` constructor.
+  /// The constructor is named after the source class, in this case, User.
+  factory UserModel.fromJson(Map<String, dynamic> json) => _$UserModelFromJson(json);
+
+  /// `toJson` is the convention for a class to declare support for serialization
+  /// to JSON. The implementation simply calls the private, generated
+  /// helper method `_$UserToJson`.
+  Map<String, dynamic> toJson() => _$UserModelToJson(this);
+
+
+
+}
---
+++ lib/models/user_model.g.dart
@@ -0,0 +1,56 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+UserModel _$UserModelFromJson(Map<String, dynamic> json) => UserModel(
+      login: json['login'] as String,
+      password: json['password'] as String,
+      name: json['name'] as String,
+      surname: json['surname'] as String,
+      birthDate: json['birthDate'] as int,
+      department: $enumDecode(_$DepartmentTypeEnumMap, json['department']),
+      project: json['project'] as String?,
+      fatherName: json['fatherName'] as String?,
+      workGrade: $enumDecodeNullable(_$WorkGradeEnumMap, json['workGrade']),
+      firstFact: json['firstFact'] as String?,
+      secondFact: json['secondFact'] as String?,
+      thirdFact: json['thirdFact'] as String?,
+    );
+
+Map<String, dynamic> _$UserModelToJson(UserModel instance) => <String, dynamic>{
+      'login': instance.login,
+      'password': instance.password,
+      'name': instance.name,
+      'surname': instance.surname,
+      'fatherName': instance.fatherName,
+      'department': _$DepartmentTypeEnumMap[instance.department]!,
+      'workGrade': _$WorkGradeEnumMap[instance.workGrade],
+      'project': instance.project,
+      'birthDate': instance.birthDate,
+      'firstFact': instance.firstFact,
+      'secondFact': instance.secondFact,
+      'thirdFact': instance.thirdFact,
+    };
+
+const _$DepartmentTypeEnumMap = {
+  DepartmentType.backend: 'backend',
+  DepartmentType.frontend: 'frontend',
+  DepartmentType.mobile: 'mobile',
+  DepartmentType.designs: 'designs',
+  DepartmentType.teamLeading: 'teamLeading',
+  DepartmentType.analytics: 'analytics',
+  DepartmentType.accounting: 'accounting',
+  DepartmentType.hr: 'hr',
+  DepartmentType.techSupport: 'techSupport',
+  DepartmentType.ads: 'ads',
+};
+
+const _$WorkGradeEnumMap = {
+  WorkGrade.junior: 'junior',
+  WorkGrade.middle: 'middle',
+  WorkGrade.senior: 'senior',
+};
